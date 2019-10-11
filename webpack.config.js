6e9c54c32a5d40cb70544153794f9bdefedcfda6
Mohand Abdalrahman <mohand.abdalrahman@mondiamedia.com>
2019-10-11 14:21:39 +0200

    configure project

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
+
+
+
+}
